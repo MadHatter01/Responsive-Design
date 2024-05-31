a7261863a665788f6027cde9aa1222b6a1149d47
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-05-30 20:04:32 -0700

    mobile and desktop view change

--- dynamic.js
+++ dynamic.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const menuBtn = document.querySelector('.menu-button');
+    const nav = document.querySelector('nav ul')
+
+    menuBtn.addEventListener('click', function(){
+        nav.classList.toggle('open')
+    })
+})
+
+
+function loadContent(){
+    const content = document.querySelector('.text-content');
+    const width = window.innerWidth;
+
+    if(width < 768){
+        content.innerHTML = '<p>Content for mobile view</p>'
+
+    }
+    else{
+        content.innerHTML = '<p>Content for desktop view</p>'
+    }
+}
+
+
 function updateImages(){
     const images = document.querySelectorAll('img[data-src-mobile]');
 
@@ -18,6 +42,7 @@ function updateImages(){
 
 
 function onResize(){
+    loadContent()
     updateImages()
 }
 
