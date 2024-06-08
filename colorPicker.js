d8a7a019fc295bff78dfbc4b4500f0ab3b97b72c
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-06-07 19:56:45 -0700

    color change logic

--- colorPicker.js
+++ colorPicker.js
@@ -1,4 +1,19 @@
-document.getElementById('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
 
     const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    const colorPicker = document.getElementById('colorPicker');
+
+    const drawShape = (color)=>{
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.fillStyle = color;
+        ctx.fillRect(50,50,200,150);
+    }
+
+    drawShape(colorPicker.value);
+
+    colorPicker.addEventListener('input', (event)=>{
+        console.log('change')
+        drawShape(event.target.value);
+    })
 })
