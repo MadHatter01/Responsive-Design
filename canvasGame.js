e8156e84fdc59d44b394a5ab98aa8627281e21b9
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-06-19 15:39:08 -0700

    collision logic and obstacles

--- canvasGame.js
+++ canvasGame.js
@@ -16,22 +16,27 @@ const drawCharacter = ()=>{
 }
 
 document.addEventListener('keydown', (event)=>{
+    console.log(score);
 switch(event.key){
     case 'ArrowUp':
         if(character.y > 0) 
             character.y -= character.speed;
+            score++;
         break;
     case 'ArrowDown':
         if(character.y + character.height < canvas.height)
             character.y += character.speed;
+            score++;
         break;
     case 'ArrowLeft':
         if(character.x > 0)
             character.x -= character.speed;
+            score++;
         break;
     case 'ArrowRight':
         if(character.x + character.width < canvas.width)
             character.x += character.speed;
+            score++;
         break;
 
 }
@@ -39,10 +44,12 @@ switch(event.key){
 
 
 const obstacles = [];
-const obstacleCount = 3;
+const obstacleCount = 30;
 
 const obstacleWidth = 20;
 const obstacleHeight = 20;
+
+let score = 0;
 
 for (let i = 0; i < obstacleCount; i++){
     obstacles.push({
@@ -62,6 +69,31 @@ const drawObstacles = ()=>{
 }
 
 
+const reset = ()=>{
+    character.x = canvas.width/2;
+    character.y = canvas.height/2;
+    score = 0;
+    obstacles.forEach(obstacle =>{
+        obstacle.x = Math.random() * (canvas.width - obstacleWidth);
+        obstacle.y = Math.random() * (canvas.height - obstacleHeight);
+    })
+}
+
+const checkCollision = () =>{
+    obstacles.forEach(obstacle =>{
+        if (character.x < obstacle.x + obstacle.width && character.x + character.width > obstacle.x && character.y < obstacle.y + obstacle.height && character.y + character.height > obstacle.y ){
+            alert('Game over!');
+            reset();
+        }
+    })
+}
+
+const drawScore = ()=>{
+    context.fillStyle = 'black';
+    context.font = "24px Roboto";
+    context.fillText('Score: '+ score, 50,50);
+}
+
 const clearCanvas = ()=>{
     context.clearRect(0,0, canvas.width, canvas.height)
 }
@@ -70,6 +102,8 @@ const animate = ()=>{
     clearCanvas();
     drawCharacter();
     drawObstacles();
+    drawScore();
+    checkCollision();
     requestAnimationFrame(animate);
 }
 
