2968e5f488c22584255da7e65fadfa7c38f4a300
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-06-18 22:35:51 -0700

    boundaries and logic

--- canvasGame.js
+++ canvasGame.js
@@ -6,7 +6,7 @@ const character = {
     y: canvas.height/2,
     width:50,
     height:50,
-    color:'blue',
+    color:'steelblue',
     speed:5
 }
 
@@ -15,8 +15,38 @@ const drawCharacter = ()=>{
     context.fillRect(character.x, character.y, character.width, character.height);
 }
 
+document.addEventListener('keydown', (event)=>{
+switch(event.key){
+    case 'ArrowUp':
+        if(character.y > 0) 
+            character.y -= character.speed;
+        break;
+    case 'ArrowDown':
+        if(character.y + character.height < canvas.height)
+            character.y += character.speed;
+        break;
+    case 'ArrowLeft':
+        if(character.x > 0)
+            character.x -= character.speed;
+        break;
+    case 'ArrowRight':
+        if(character.x + character.width < canvas.width)
+            character.x += character.speed;
+        break;
+
+}
+})
+
+
+
+const clearCanvas = ()=>{
+    context.clearRect(0,0, canvas.width, canvas.height)
+}
+
 const animate = ()=>{
+    clearCanvas();
     drawCharacter();
+    requestAnimationFrame(animate);
 }
 
 animate();
